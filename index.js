b1d7e913353c7309b93d7fbefc1a080df59bb6b8
Kamil Albrycht <ponciusz@gmail.com>
2018-10-31 11:02:31 +0000

    code improvement

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const userdb = JSON.parse(fs.readFileSync("./users.json", "UTF-8"));
 // openssl genrsa -out private.pem 2048
 // # extatract public key from it
 // openssl rsa -in private.pem -pubout > public.pem
-const privateCert = fs.readFileSync('private.pem');
-const publicCert = fs.readFileSync('public.pem');
+const privateCert = fs.readFileSync("private.pem");
+const publicCert = fs.readFileSync("public.pem");
 
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
@@ -23,7 +23,7 @@ server.use(jsonServer.defaults());
 
 // Verify the token
 function verifyToken(token, res, next) {
-  jwt.verify(token, publicCert, function(err, decoded) {
+  jwt.verify(token, publicCert, function(err) {
     if (err) {
       return res.status(401).json({
         status: 401,
@@ -35,7 +35,7 @@ function verifyToken(token, res, next) {
 }
 
 // Check if the user exists in database
-function isAuthenticated({ email, password }) {
+function isAuthenticated(email, password) {
   return (
     userdb.users.findIndex(
       user => user.email === email && user.password === password
@@ -45,54 +45,63 @@ function isAuthenticated({ email, password }) {
 
 server.post("/auth/login", (req, res) => {
   const postData = req.body;
-  const user = {
-    email: postData.email,
-    password: postData.password
-  };
+  const { email, password } = postData;
 
-  if (isAuthenticated(user) === false) {
+  if (isAuthenticated(email, password) === false) {
     const status = 401;
     const message = "Incorrect email or password";
     res.status(status).json({ status, message });
     return;
   }
 
-  const accessToken = jwt.sign({email: postData.email}, privateCert, {
-    algorithm: 'RS256',
+  // Use Object.assign to avoid mutation of the object
+  const userData = Object.assign(
+    {},
+    userdb.users.find(function(element) {
+      return element.email === postData.email;
+    })
+  );
+  delete userData.password;
+
+  const accessToken = jwt.sign({ id: userData.id }, privateCert, {
+    algorithm: "RS256",
     expiresIn: config.TOKEN_LIFE
   });
-  const refreshToken = jwt.sign({email: postData.email}, privateCert, {
-    algorithm: 'RS256',
+  const refreshToken = jwt.sign({ id: userData.id }, privateCert, {
+    algorithm: "RS256",
     expiresIn: config.REFRESH_TOKEN_LIFE
   });
 
   const response = {
     accessToken,
     refreshToken,
-    email: user.email
+    ...userData
   };
-
-  tokenList[refreshToken] = response;
+  // Update refresh token in memory so server can invalidate it anytime
+  // In this example nodemon restart cause clearing it
+  tokenList[refreshToken] = {
+    id: userData.id
+  };
   res.status(200).json(response);
 });
 
 server.post("/auth/token", (req, res) => {
   const postData = req.body;
+  const {refreshToken} = postData;
 
-  if (postData.refreshToken && postData.refreshToken in tokenList) {
-    const user = {
-      email: postData.email,
-      name: postData.name
-    };
-    const token = jwt.sign({email: postData.email}, privateCert, {
-      algorithm: 'RS256',
-      expiresIn: config.TOKEN_LIFE
-    });
+  if (refreshToken && refreshToken in tokenList) {
+    const token = jwt.sign(
+      { id: tokenList[refreshToken].id }, // use id from memory
+      privateCert,
+      {
+        algorithm: "RS256",
+        expiresIn: config.TOKEN_LIFE
+      }
+    );
     const response = {
       token: token
     };
-    // update the token in the list
-    tokenList[postData.refreshToken].token = token;
+
     res.status(200).json(response);
   } else {
     res.status(404).send("Invalid request");
